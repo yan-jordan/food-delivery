04ae6bbae354e452a4ed242ab78f58bbd7ee76be
pouyan mehdibeik <pouyanmb@pouyans-MacBook-Pro.local>
2025-08-06 07:12:47 +0330

    make components of home page responsive with @media(max-widht: )

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import {assets} from "../../assets/assets"
 import { useState } from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     
@@ -11,10 +12,10 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={assets.logo} className='logo'/>
         <ul className='navbar-menu'>
-            <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
-            <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
-            <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile App</li>
-            <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>About Us</li>
+            <Link to = '/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
+            <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
+            <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile App</a>
+            <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>About Us</a>
         </ul>
 
         <div className="navbar-right">
